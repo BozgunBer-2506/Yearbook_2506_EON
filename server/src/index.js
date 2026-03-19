49ec2ae71fb4636e40de040029bdfd9d067a339c
BozgunBer-2506 <bozgun76@gmail.com>
2026-03-19 11:15:40 +0100

    fix: manual CORS preflight handler

--- server/src/index.js
+++ server/src/index.js
@@ -313,10 +313,19 @@ app.use(cors({
     'https://yearbook2506eon-production.up.railway.app',
     'https://yearbook2506eon.up.railway.app'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options("*", cors());
+// Preflight for all routes
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 
